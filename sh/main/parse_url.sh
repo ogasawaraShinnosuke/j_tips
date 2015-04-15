7bd243840e36aefe0379f9133e21a54a7e3b5dd9
ogasawaraShinnosuke <ogasawaraShinnosuke@users.noreply.github.com>
2015-04-15 15:36:01 +0900

    Update parse_url.sh

--- sh/main/parse_url.sh
+++ sh/main/parse_url.sh
@@ -22,3 +22,9 @@ RUN()
 
 # run
 RUN $0
+
+#complex
+#!/bin/bash
+LIST=./hoge.txt
+J_REGEX=[ぁ-んァ-ヶ一-龠々ー]
+FUNC_PARSE(){ while read line;do echo $line|awk '{if(NF==2&&$1~/[0-9]*/&&$2~/^http/)print$0}'|sed -e 's/\t.*\(^http\)/\1/g' -e 's/$J_REGEX//g' >>parsed_list;done <${LIST};};FUNC_PARSE $0
